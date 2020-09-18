2894dcb0ddd227f325ca949ab125e08da022790e
Luca Briguglia <luca.briguglia@outlook.com>
2020-09-18 16:53:59 +0100

    Update attributes

--- src/Docs/Attributes/DocRequestAttribute.cs
+++ src/Docs/Attributes/DocRequestAttribute.cs
@@ -2,21 +2,11 @@
 
 namespace Docs.Attributes
 {
-    /// <summary>
-    /// 
-    /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
     public class DocRequestAttribute : Attribute
     {
-        /// <summary>
-        /// 
-        /// </summary>
         public Type Target { get; }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="target"></param>
         public DocRequestAttribute(Type target)
         {
             Target = target;
--- src/Docs/Attributes/DocTargetAttribute.cs
+++ src/Docs/Attributes/DocTargetAttribute.cs
@@ -2,28 +2,15 @@
 
 namespace Docs.Attributes
 {
-    /// <summary>
-    /// 
-    /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
     public class DocTargetAttribute : Attribute
     {
-        /// <summary>
-        /// 
-        /// </summary>
         public string Context { get; }
 
-        /// <summary>
-        /// 
-        /// </summary>
         public DocTargetAttribute()
         {
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="context"></param>
         public DocTargetAttribute(string context)
         {
             Context = context;
